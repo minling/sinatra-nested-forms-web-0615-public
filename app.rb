b7c1c2883901c21457a5420c516ccbf3aaf61869
minling <mzhao725@gmail.com>
2015-07-05 00:47:50 -0400

    done

--- app.rb
+++ app.rb
@@ -4,6 +4,19 @@ module FormsLab
   class App < Sinatra::Base
 
     # code other routes/actions here
+    get '/' do 
+      erb :root
+    end
+
+    get '/new' do #enters new pirate
+      erb :'pirates/new'
+    end
+
+    post '/pirates' do
+      @pirates = params[:pirate]
+
+      erb :'/pirates/show'
+    end
 
   end
 end
